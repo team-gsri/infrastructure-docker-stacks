c091d7e3505cc71e8202b8b868b085f2b78db197
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2023-05-08 06:24:58 +0200

    fix(cloud): cron script container selection

--- stacks/cloud/cron.sh
+++ stacks/cloud/cron.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 SERVICE=cloud_nextcloud
-TASK=`docker service ps $SERVICE -q`
+TASK=`docker service ps --filter "desired-state=running" --quiet $SERVICE`
 CONTAINER=$SERVICE.1.$TASK
 
 docker exec --user www-data $CONTAINER /usr/local/bin/php /var/www/html/cron.php
